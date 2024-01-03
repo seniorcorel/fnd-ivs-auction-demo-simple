0030c1a08aee6b3d6467ac83c43450d6aaf25944
saayaman <ayako@fournine.digital>
2024-01-03 09:41:25 -0800

    update

--- src/app/api/getStream/route.js
+++ src/app/api/getStream/route.js
@@ -5,6 +5,7 @@ export async function GET() {
   const client = new IvsClient({
     region: process.env.NEXT_PUBLIC_AWS_REGION,
   })
+
   const command = new GetStreamCommand({ channelArn: process.env.NEXT_PUBLIC_CHANNEL_ARN })
   const { stream } = await client.send(command)
   return Response.json(stream);
